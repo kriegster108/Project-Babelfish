2b5dfd4a9b3c1a0ae58dbfcb0444fe5f5b20d7ba
Cassidy Antic <cassidy.antic@ihsmarkit.com>
2021-02-25 10:17:30 -0700

    added header

--- babel-fish/src/app/app.module.ts
+++ babel-fish/src/app/app.module.ts
@@ -5,16 +5,24 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home-component/home-component.component';
 import { PageWrapperComponent } from './pages/page-wrapper-component/page-wrapper-component.component';
+import { HeaderComponent } from './pages/header/header.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    PageWrapperComponent
+    PageWrapperComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
