ccccd298280f7f1381da2214866d450deedc97df
Dustin Mikusko <dustin.mikusko@ihsmarkit.com>
2021-02-25 11:21:15 -0700

    Add translation service. Add skeleton for endpoints to use translation service dependency

--- TranslationStation/TranslationStation/Controllers/TranslationsController.cs
+++ TranslationStation/TranslationStation/Controllers/TranslationsController.cs
@@ -4,20 +4,29 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using TranslationStation.Services;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace TranslationStation.Controllers
 {
+    
     [Route("api/[controller]")]
     [ApiController]
     public class TranslationsController : ControllerBase
     {
+        private readonly ITranslationService _translationService;
+        public TranslationsController(ITranslationService translationService)
+        {
+            _translationService = translationService;
+        }
+
         // GET api/translations
         // Return translations for a specific language
         [HttpGet("{languageId}")]
         public string Translations(string languageId)
         {
+            var translations = _translationService.GetTranslations(languageId);
             return "value";
         }
 
@@ -27,6 +36,7 @@ namespace TranslationStation.Controllers
         [HttpGet("{languageId}")]
         public string UnverifiedTranslation(string languageId)
         {
+            var translations = _translationService.GetUnverifiedTranslations(languageId);
             return "value";
         }
 
@@ -35,6 +45,7 @@ namespace TranslationStation.Controllers
         [HttpPatch("{languageId}/{translationKey}")]
         public string VerifyTranslation([FromBody] string value, string languageId, string translationKey)
         {
+            var translations = _translationService.VerifyTranslation(languageId);
             return "value";
         }
 
@@ -43,6 +54,7 @@ namespace TranslationStation.Controllers
         [HttpPost]
         public void Post([FromBody] string value)
         {
+            var translations = _translationService.Translate(value);
         }
     }
 }
