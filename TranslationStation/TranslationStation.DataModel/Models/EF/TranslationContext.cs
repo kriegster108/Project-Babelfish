75f873b65921897988d1fe082d696ec5abaf4f83
jim.dillon <jim.dillon@ihsmarkit.com>
2021-02-25 15:42:46 -0700

    TranslationStation DbContext Connection string
    Added username/pw to connection to pgsql string

--- TranslationStation/TranslationStation.DataModel/Models/EF/TranslationContext.cs
+++ TranslationStation/TranslationStation.DataModel/Models/EF/TranslationContext.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System;
+using System.ComponentModel.DataAnnotations;
 using Microsoft.EntityFrameworkCore;
 
 namespace TranslationStation.DataModel.Models.EF
